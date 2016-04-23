7a72493357c21154ccc7e1631295ed7761e9e49c
Ben Klein <robobenklein@gmail.com>
2016-04-22 20:36:07 -0400

    Missing character

--- puppet/modules/blenderfarm/files/blender-slave.py
+++ puppet/modules/blenderfarm/files/blender-slave.py
@@ -5,7 +5,7 @@ import netrender
 from netrender.utils import *
 bpy.data.scenes["Scene"].render.engine = "NET_RENDER"
 # for slave
-py.data.scenes["Scene"].network_render.mode = "RENDER_SLAVE"
+bpy.data.scenes["Scene"].network_render.mode = "RENDER_SLAVE"
 bpy.data.scenes["Scene"].network_render.server_address = "jabba-masta-00.knoxschools.ad"
 # "knox" on a keypad
 bpy.data.scenes["Scene"].network_render.server_port = 5669
