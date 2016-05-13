a2373dbe742bbb308e424f1a91e093d54853a299
Ben Klein <robobenklein@gmail.com>
2016-05-13 16:37:19 -0400

    Slave tags with versions!

--- puppet/modules/blenderfarm/files/blender-slave.py
+++ puppet/modules/blenderfarm/files/blender-slave.py
@@ -10,5 +10,10 @@ bpy.data.scenes["Scene"].network_render.server_address = "jabba-masta-00.knoxsch
 # "knox" on a keypad
 bpy.data.scenes["Scene"].network_render.server_port = 5669
 
+bpy.data.scenes["Scene"].network_render.use_slave_thumb = True
+bpy.data.scenes["Scene"].network_render.slave_tags = "headless" + 
+
+bpy.data.scenes["Scene"].network_render.slave_tags = bpy.data.scenes["Scene"].network_render.slave_tags + str(bpy.app.version[0]) + "." + str(bpy.app.version[1]) + str(bpy.app.version_char)
+
 # then
 bpy.ops.render.netclientstart()
