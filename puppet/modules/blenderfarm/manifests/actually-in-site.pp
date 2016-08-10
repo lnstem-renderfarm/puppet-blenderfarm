e1abc978bde7e6683cfd325c9d733143ed56317a
Ben Klein <robobenklein@gmail.com>
2016-08-10 11:58:11 -0400

    Fix node definitions

---
+++ puppet/modules/blenderfarm/manifests/actually-in-site.pp
@@ -0,0 +1,33 @@
+node default {
+  include blenderfarm
+}
+
+
+node 'jabba-masta-00.knoxschools.ad' {
+  include blenderfarm
+
+  class { 'puppetdb': }
+  class { 'puppetdb::master::config': }
+
+  # Configure Apache
+  # Ensure it does *not* purge configuration files
+  class { 'apache':
+    purge_configs => false,
+    mpm_module    => 'prefork',
+    default_vhost => true,
+    default_mods  => false,
+  }
+
+  class { 'apache::mod::wsgi': }
+
+  # Configure Puppetboard
+  class { 'puppetboard':
+    manage_git        => true,
+    manage_virtualenv => true,
+    reports_count     => 40,
+  }
+
+  # Access Puppetboard from example.com/puppetboard
+  class { 'puppetboard::apache::conf': }
+
+}
--- puppet/modules/blenderfarm/manifests/init.pp
+++ puppet/modules/blenderfarm/manifests/init.pp
@@ -4,35 +4,3 @@ class blenderfarm {
   include blenderfarm::blender
 
 }
-
-node default {
-  include blenderfarm
-}
-
-node 'jabba-masta-00.knoxschools.ad' {
-
-  class { 'puppetdb': }
-  class { 'puppetdb::master::config': }
-
-  # Configure Apache
-  # Ensure it does *not* purge configuration files
-  class { 'apache':
-    purge_configs => false,
-    mpm_module    => 'prefork',
-    default_vhost => true,
-    default_mods  => false,
-  }
-
-  class { 'apache::mod::wsgi': }
-
-  # Configure Puppetboard
-  class { 'puppetboard':
-    manage_git        => true,
-    manage_virtualenv => true,
-    reports_count     => 40,
-  }
-
-  # Access Puppetboard from example.com/puppetboard
-  class { 'puppetboard::apache::conf': }
-
-}
